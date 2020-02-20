0889e6a7eb56d10c3e4f7db6dc46d55bcc8df5d5
alppacat <queletzu@datank.ai>
2020-02-20 00:08:58 -0600

    [ADD] home page desktop version

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Typewriter from "typewriter-effect";
 
-import { MainContainer } from './styles';
+import { MainContainer, TitleContainer, Quote, Subtitle, Button } from './styles';
 
 
 function HomePage(){
@@ -10,19 +10,26 @@ const [loading, setLoading] = useState(true);
 
     return(
         <MainContainer>
-            <Typewriter 
-                onInit={(typewriter) => {
-                    typewriter.typeString('Tzu Pratts')
-                    .callFunction(()=> {
-                        setLoading(false)
-                    })
-                    .pauseFor(2500)
-                    .start();
-                }}
-            />
+            <TitleContainer>
+                <Typewriter 
+                    onInit={(typewriter) => {
+                        typewriter.typeString('Tzu Pratts')
+                        .callFunction(()=> {
+                            setLoading(false)
+                        })
+                        .pauseFor(2500)
+                        .start();
+                    }}
+                />
+            </TitleContainer>
             {
                 !loading &&
-                <p>"Any sufficiently advanced technology is indistinguishable from magic" <br/> -Arthur C. Clarke</p>
+                <React.Fragment>
+                    <Subtitle>Front-end Developer</Subtitle>
+                    <Quote>"Any sufficiently advanced technology is indistinguishable from magic" <br/> -Arthur C. Clarke</Quote>
+                    <Button>PRESS TO ENTER</Button>
+                </React.Fragment>
+                
             }
         </MainContainer>
     )
--- src/containers/HomePage/styles.js
+++ src/containers/HomePage/styles.js
@@ -1,6 +1,119 @@
 import styled from 'styled-components';
+import bg from '../../assets/bg.jpg'
 
 export const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100vw;
     height: 100vh;
+    background: radial-gradient(
+        circle at bottom,
+        rgb(139, 0, 91, 0.9),
+        rgb(96, 26, 85, 0.9),
+        rgb(58, 29, 69, 0.9),
+        rgb(30, 24, 44, 0.9),
+        rgb(12, 12, 14, 0.9)
+      ), url(${bg});
+    background-size: cover;
+    background-repeat: no-repeat; 
 `;
+
+export const TitleContainer = styled.div`
+    margin-top: 6rem;
+    color: #d90474;
+    text-shadow: 2px 2px #00bfa8;
+    font-size: 9rem;
+    font-weight: 900;
+    font-family: "VT323", monospace;
+
+`
+export const Subtitle = styled.p`
+    font-size: 3rem;
+    font-family: "Space Mono", monospace;
+    color: #00bfa8;
+    text-shadow: 2px 2px #d90474;
+    font-weight: bolder;
+    margin: 0;
+    animation: slow .9s 1 0s ease;
+    
+    @keyframes slow{
+        0%{
+        opacity: 0;
+        }
+        70%{
+            opacity: 0.3;
+        }
+        80%{
+            opacity: 0.5;
+        }
+        90%{
+            opacity: 0.8;
+        }
+    }
+
+`
+export const Quote = styled.p`
+    font-family: "Space Mono", monospace;
+    text-align: center;
+    color: #008b68;
+    text-shadow: none;
+    margin-bottom: 6em;
+    margin-top: 3em;
+    animation: slow .9s 1 0s ease;
+
+    @keyframes slow{
+        0%{
+        opacity: 0;
+        }
+        70%{
+            opacity: 0.3;
+        }
+        80%{
+            opacity: 0.5;
+        }
+        90%{
+            opacity: 0.8;
+        }
+    }
+`
+export const Button = styled.div`
+    padding: 1.5rem;
+    font-size: 1.5rem;
+    background-image: linear-gradient(0deg,#e3b943,#e3ca4b,#e2dc56,#deed63,#d9ff74);
+    box-shadow: 1px 3px #d90474;
+    border-radius: 2rem;
+    font-weight: bolder;
+    color: #3f1942;
+    text-decoration: none;
+    font-family: VT323,monospace;
+    text-shadow: 2px 2px #00bfa8;
+    animation: slow .9s 1 0s ease;
+
+    :hover{
+    cursor: pointer;
+    color: #3f1942;
+    background-image: linear-gradient(
+      to bottom,
+      #e3b943,
+      #e3ca4b,
+      #e2dc56,
+      #deed63,
+      #d9ff74
+    );
+
+    @keyframes slow{
+        0%{
+        opacity: 0;
+        }
+        70%{
+            opacity: 0.3;
+        }
+        80%{
+            opacity: 0.5;
+        }
+        90%{
+            opacity: 0.8;
+        }
+  }
+`
